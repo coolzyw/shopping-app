5231d79e398e31445821943e33be8779da25e3ce
Yiwei Zhang <happyzyw@umich.edu>
2020-04-19 12:16:21 -0500

    displayed images

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,14 +7,16 @@ const Item = ({ product }) => {
         <ul>
             <Card>
                 <Card.Image>
+                    <img src={require(`../../static/${product.sku}_1.jpg`)} alt="Smiley face" />
                 </Card.Image>
+
                 <Card.Header>
                     {product.title}
                     <Card.Header.Title>
                         {product.price}
                     </Card.Header.Title>
-
                 </Card.Header>
+
                 <Card.Content>
                     {product.description}
                 </Card.Content>
